493ad419b088899459895d42450797a02e69a215
Thanatos666 <69635586+Thanatos9911@users.noreply.github.com>
2020-10-28 23:11:55 +0900

    practice end

--- MySQL/practice/php/create table/main.php
+++ MySQL/practice/php/create table/main.php
@@ -16,23 +16,38 @@
 <?php
 
 $password = null;
-
 if (isset($_GET["comment"])) {
     $password = $_GET["comment"];
-    if (mysqli_connect("localhost","root",$password)) {
+    $link = mysqli_connect("localhost","root",$password);
+    if ($link) {
     }
     else {
-        echo "connect fail!";
+        die ('mysql connect faild<span class="br"></span>');
     }
 }
 else {
     print "ERROR";
 }
+$link = mysqli_connect("localhost","root",$password);
 
-$sql = 'create table (
-    id int(11) auto_increment not null primary key,
-    email varchar(20)
-);';
+$db_se = mysqli_select_db($link, 'php_prac');
+if (!$db_se) {
+    die ('db connect faild<span class="br"></span>');
+}
+
+$sql = 'select * from table1';
+
+$result = mysqli_query($link, $sql);
+if (!$result) {
+    die ('query faild<span class="br"></span>');
+}
+
+echo "<h3>data of php_prac.table1</h3>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "id = ".$row['id']."   name = ".$row['name'].'<span class="br"></span>';
+}
+
+mysqli_close($link);
 
 ?>
             <h3>after php</h3>
