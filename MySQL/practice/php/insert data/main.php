ba24230568a7f7ad070bd8cb5160aa9ec9429bf7
Thanatos9911 <thanatos9911@gmail.com>
2020-10-29 22:44:40 +0900

    create new practice

---
+++ MySQL/practice/php/insert data/main.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charlist="utf-8">
+        <title>practice</title>
+        <link rel="stylesheet" href="/Templates/PHP prac/doc_style.css">
+    </head>
+    <body>
+        <a href="./.." class="parent_dir">Parent directory</a>
+        <div class="main">
+            <h1 class="title">php practice</h1>
+            <section class="html" id="prev">
+            </section>
+            <section class="php">
+            <h3>befor php</h3>
+<?php
+
+$password = null;
+
+
+$password = $_GET["pass"];
+if (mysqli_connect("localhost","root",$password)) {
+    echo "connect success!";
+}
+else {
+    echo "connect fail!";
+}
+
+?>
+            <h3>after php</h3>
+            </section>
+            <section class="html" id="after">
+            </section>
+        </div>
+    </body>
+</html>
